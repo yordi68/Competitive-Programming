4e2a347899c4f362f836ff14861ad249be7f9561
yordi68 <108049153+yordi68@users.noreply.github.com>
2022-09-27 19:23:30 +0300

    Time: 231 ms (92.37%), Space: 25.4 MB (75.69%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,9 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        while k != 0:
-            a = nums.pop()
-            nums.insert(0,a)
-            k -= 1
+        k = k % len(nums)
+        l,r = 0 , len(nums) - 1
+        while l < r:
+            nums[l],nums[r] = nums[r],nums[l]
+            l , r = l + 1,r - 1
+        l,r = 0 , k - 1
+        while l < r:
+            nums[l],nums[r] = nums[r],nums[l]
+            l , r = l + 1,r - 1
+        l,r = k , len(nums) - 1
+        while l < r:
+            nums[l],nums[r] = nums[r],nums[l]
+            l , r = l + 1,r - 1
 
         
