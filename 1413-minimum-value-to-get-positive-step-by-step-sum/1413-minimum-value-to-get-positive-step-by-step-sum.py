e694f9e201535ec6aae76e74bdbec640fb2b0ca8
yordi68 <108049153+yordi68@users.noreply.github.com>
2022-10-04 02:43:51 -0400

    Time: 65 ms (29.89%), Space: 13.8 MB (55.50%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
@@ -1,10 +1,3 @@
 class Solution:
     def minStartValue(self, nums: List[int]) -> int:
-        nums = list(itertools.accumulate(nums))
-        print(nums)
-        if min(nums) < 0:
-            a = min(nums)
-            a = (a * (-1)) + 1
-            return(a)
-        else:
-            return(1)
+        return 1 - min(accumulate(nums,initial=0))
