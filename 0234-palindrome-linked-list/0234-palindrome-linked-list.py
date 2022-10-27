5e4958edc035822fea203809a703d807e0ef4569
yordi68 <108049153+yordi68@users.noreply.github.com>
2022-10-27 01:39:58 -0400

    Time: 1161 ms (72.80%), Space: 39 MB (78.18%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,23 +5,20 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        fast = slow = head
-        # find the middlle index
+        slow = fast = head
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
-        # reverse secong half
         prev = None
         while slow:
-            tmp = slow.next
+            temp = slow.next
             slow.next = prev
             prev = slow
-            slow = tmp
-        # check palindrome
-        l,r = head,prev
-        while r:
-            if l.val != r.val:
+            slow = temp
+        left , right = head , prev
+        while right:
+            if left.val != right.val:
                 return False
-            l = l.next
-            r = r.next
+            left = left.next
+            right = right.next
         return True
