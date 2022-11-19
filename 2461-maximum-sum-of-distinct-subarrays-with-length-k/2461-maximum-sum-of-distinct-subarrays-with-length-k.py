77daef815dfb6753a5c35fbda52d3d6daa86c332
yordi68 <108049153+yordi68@users.noreply.github.com>
2022-11-19 11:29:39 -0500

    Time: 786 ms (98.45%), Space: 28.7 MB (96.41%) - LeetHub

---
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        res, cur, pos, dup = 0, 0, [-1] * 100001, -1
+        
+        for i in range(0,len(nums)):
+            cur += nums[i]                      # compute running sum for
+            if i >= k: cur -= nums[i-k]         # the window of length k
+            
+            dup = max(dup, pos[nums[i]])        # update LAST seen duplicate
+            
+            if i - dup >= k:                    # if no duplicates were found
+                res = max(res, cur)             # update max window sum
+
+            pos[nums[i]] = i
+
+        return res
