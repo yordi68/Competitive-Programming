814f538cce8c0a6342a29a892236e7f1b6b62f01
yordi68 <108049153+yordi68@users.noreply.github.com>
2022-12-24 09:54:46 +0300

    Time: 751 ms (88.37%), Space: 19.2 MB (66.84%) - LeetHub

---
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
@@ -0,0 +1,25 @@
+class Solution:
+    # def manhattan_distance(num1, num2, x, y):
+    #     length = (abs(num1 - x) + abs(num2 - y))
+    #     return length
+    def nearestValidPoint(self, x: int, y: int, points: List[List[int]]) -> int:
+        
+        
+        minimum_distance = float("inf")
+        minimum_distance_index = -1
+        n = len(points)
+        
+        for i in range(n):
+            num1,num2 = points[i]
+            if num1 == x or num2 == y:
+                length = (abs(num1 - x) + abs(num2 - y))
+                if length < minimum_distance:
+                    minimum_distance = length
+                    minimum_distance_index = i
+#                 elif length == minimum_distance:
+#                     if num1 < minimum_distance_x:
+#                         minimum_distance_index = i
+#                         minimum_distance_x = num1
+                        
+        return (minimum_distance_index)
+    
