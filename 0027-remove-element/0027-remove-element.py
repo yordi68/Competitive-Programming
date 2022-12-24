4d016fd8d6a652324d5178ebef855339d4b13a72
yordi68 <108049153+yordi68@users.noreply.github.com>
2022-12-24 10:47:34 +0300

    Time: 32 ms (95.30%), Space: 13.8 MB (63.32%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -7,35 +7,19 @@ class Solution:
         count = 0
 
         while left <= right:
+            # pass the number if it is not equal to value
             while left <= right and nums[left] != val:
                 left += 1
-                
+            # pass the number if it is equal to the value   
             while left <= right and nums[right] == val:
                 count += 1
                 right -= 1
-            
+            # swap the value at the left and the right pointer
             if left <= right:
                 nums[left], nums[right] = nums[right] , nums[left]
+                count += 1
                 left += 1
                 right -= 1
-                count += 1
-            #left < n and value at left != val
-            #left += 1
-            
-            
-            # if nums[left] == val and nums[right] != val:
-            #     nums[left], nums[right] = nums[right], nums[left]
-            #     count += 1
-            #     left += 1
-            #     right -= 1
-            # elif nums[right] == val and nums[left] != val:
-            #     count += 1
-            #     right -= 1
-            # elif nums[right] != val and nums[left] != val:
-            #     left += 1
-            # elif nums[right] == val and nums[left] == val:
-            #     count += 1
-                # right -= 1
-        #3  3   3
-        
+                
+        # return the lenght of the nums minus the value of the count
         return n - count
