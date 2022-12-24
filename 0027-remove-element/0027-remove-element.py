b64e4ec0da7d104e697639776f41957e3f7ac3fb
yordi68 <108049153+yordi68@users.noreply.github.com>
2022-12-24 10:43:06 +0300

    Time: 40 ms (80.43%), Space: 13.9 MB (63.32%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -7,19 +7,35 @@ class Solution:
         count = 0
 
         while left <= right:
-            if nums[left] == val and nums[right] != val:
-                nums[left], nums[right] = nums[right], nums[left]
+            while left <= right and nums[left] != val:
+                left += 1
+                
+            while left <= right and nums[right] == val:
                 count += 1
+                right -= 1
+            
+            if left <= right:
+                nums[left], nums[right] = nums[right] , nums[left]
                 left += 1
                 right -= 1
-            elif nums[right] == val and nums[left] != val:
                 count += 1
-                right -= 1
-            elif nums[right] != val and nums[left] != val:
-                left += 1
-            elif nums[right] == val and nums[left] == val:
-                count += 1
-                right -= 1
-        
+            #left < n and value at left != val
+            #left += 1
+            
+            
+            # if nums[left] == val and nums[right] != val:
+            #     nums[left], nums[right] = nums[right], nums[left]
+            #     count += 1
+            #     left += 1
+            #     right -= 1
+            # elif nums[right] == val and nums[left] != val:
+            #     count += 1
+            #     right -= 1
+            # elif nums[right] != val and nums[left] != val:
+            #     left += 1
+            # elif nums[right] == val and nums[left] == val:
+            #     count += 1
+                # right -= 1
+        #3  3   3
         
         return n - count
