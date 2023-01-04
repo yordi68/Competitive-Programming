38fccc9592b0a24d7bcfac0e39b81567fd7651a4
yordi68 <108049153+yordi68@users.noreply.github.com>
2023-01-04 11:30:57 +0300

    Time: 58 ms (7.07%), Space: 14 MB (35.43%) - LeetHub

--- 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.py
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.py
@@ -1,15 +1,13 @@
 class Solution:
     def countDigits(self, num: int) -> int:
-        num_list = []
         count = 0
         temp = num
         while temp > 0:
-            num_list.append(temp % 10)
+            rem = temp % 10
+            if num % rem == 0:
+                count += 1
+            
             temp = temp // 10
             
-        n = len(num_list)
-        for i in range(n):
-            if num % num_list[i] == 0:
-                count += 1
                 
         return count
