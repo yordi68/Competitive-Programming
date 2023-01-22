8e28b3f278bc8bf4d37ef8a58824599ac22a9169
yordi68 <108049153+yordi68@users.noreply.github.com>
2023-01-22 15:45:40 +0300

    Time: 657 ms (60.59%), Space: 26.6 MB (45.48%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
@@ -1,19 +1,14 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
-        piles.sort()
-        n = len(piles)
-        count = 0
+        piles.sort(reverse = True)
         total = 0
-        r = n
-        c = 0
-        itr = n // 3
-        for i in range(n):
-            count += 1
-            r -= 1
-            if count == 2 and itr != c:
-                c += 1
-                total += piles[r]
-                count = 0
-                
+        n = len(piles) // 3
+        i = 1
+        
+        for _ in range(n):
+            total += piles[i]
+            i += 2
+            
         return total
+
         
