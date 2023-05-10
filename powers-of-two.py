ab4979a194bcf9eb3b57708951927bb9292ae982
Yordanos Yirgu <yordanos.yirgu@aastustudent.edu.et>
2023-05-10 14:52:36 +0300

    added powers-of-two.py

---
+++ powers-of-two.py
@@ -0,0 +1,26 @@
+def solve():
+    n, k = map(int, input().split())
+    if k > n:
+        print("NO")
+        return
+
+    ans = []
+    for i in range(32):
+        if n & (1 << i):
+            ans.append(1 << i)
+
+    if len(ans) > k:
+        print("NO")
+        return
+
+    print("YES")
+    i = 0
+    while len(ans) < k:
+        while ans[i] == 1:
+            i += 1
+        ans[i] //= 2
+        ans.append(ans[i])
+    print(*sorted(ans))
+
+
+solve()
