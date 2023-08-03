a93538b033bcae0a302f49f02e4f786e8714c1b5
Yordanos Yirgu <yordanos.yirgu@aastustudent.edu.et>
2023-08-03 17:01:43 +0300

    added furthest-building-you-can-reach.python

---
+++ furthest-building-you-can-reach.py
@@ -0,0 +1,16 @@
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        heap = []
+
+        for i in range(len(heights) - 1):
+
+            diff =  heights[i + 1] - heights[i]
+            if diff > 0:
+                heapq.heappush(heap, diff)
+                if len(heap) > ladders:
+                    minHeight = heapq.heappop(heap)
+                    bricks -= minHeight
+                if bricks < 0:
+                    return i
+        
+        return len(heights) - 1
