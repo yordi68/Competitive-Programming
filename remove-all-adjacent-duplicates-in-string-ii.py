a98b9386c1147297c43c8d422a6aafc7dbebae3b
Yordanos Yirgu <yordanos.yirgu@aastustudent.edu.et>
2023-10-04 15:15:05 +0300

    added remove-all-adjacent-duplicates-in-string-ii.python

---
+++ remove-all-adjacent-duplicates-in-string-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = []
+
+        for char in s:
+            if stack and stack[-1][0] == char:
+                stack[-1][1] += 1
+                if stack[-1][1] == k:
+                    stack.pop()
+            else:
+                stack.append([char, 1])
+
+                
+        ans = ""
+        for char, cnt in stack:
+            ans += (char * cnt)
+        return ans
