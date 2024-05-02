11afa6786c59043435dfe7ff30c2afb9a0fef917
Yordanos Yirgu <yordanos.yirgu@aastustudent.edu.et>
2024-05-02 06:20:46 +0300

    Time: 748 ms (58.69%) | Memory: 27.3 MB (90.71%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        """
-        maxDiff = 5
-            | 1,  
-            if incoming > there => subrtract them and give the value to maxDiff
-            if incoming < there => pop out of the stack
-            | 1, 
         
-        """
-
         stack = []
         maxDiff = 0
         stack.append(prices[0])
+
+
         for price in prices:
             if price > stack[-1]:
                 maxDiff = max(maxDiff, price - stack[-1])
