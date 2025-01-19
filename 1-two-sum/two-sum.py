e9ea024b16c3c43a9cc79a11b14402e174f4aae4
Yordanos Yirgu <yordanos.yirgu@aastustudent.edu.et>
2025-01-19 06:59:45 +0300

    Time: 7 ms (37.86%) | Memory: 19.4 MB (7.27%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -11,13 +11,13 @@ class Solution:
         nums_index = sorted(nums_index, key=lambda x: x[0])
 
         while left != right:
-            print(nums_index[left][0])
-            print(nums_index[right][0])  
 
             if nums_index[left][0] + nums_index[right][0] == target:
                 return [nums_index[left][1], nums_index[right][1]]
+
             elif nums_index[left][0] + nums_index[right][0] > target:
                 right -= 1
+                
             else:
                 left += 1
 
