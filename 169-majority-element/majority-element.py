828993a5b41307535c497ca4662c394e3ea96a98
Yordanos Yirgu <yordanos.yirgu@aastustudent.edu.et>
2025-01-22 07:09:06 +0300

    Time: 11 ms (31.68%) | Memory: 19.5 MB (12.61%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hashmap = defaultdict(int)
-        n = len(nums)
+        count = defaultdict(int)
 
-        for number in nums:
-            hashmap[number] += 1
-            if hashmap[number] > floor(n / 2):
-                return number
-
+        for i in range(len(nums)):
+            count[nums[i]] += 1
         
+        return max(count, key=count.get)
