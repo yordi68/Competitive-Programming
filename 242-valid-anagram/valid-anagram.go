c5fb1bbe112423d3ebeeea0cb3a6fbcd7177dac9
Yordanos Yirgu <yordanos.yirgu1@gmail.com>
2025-08-05 23:24:14 +0300

    Time: 5 ms (48.67%) | Memory: 4.8 MB (74.02%) - LeetSync

--- 242-valid-anagram/valid-anagram.go
+++ 242-valid-anagram/valid-anagram.go
@@ -1,24 +1,22 @@
 func isAnagram(s string, t string) bool {
-
     if len(s) != len(t) {
         return false
     }
 
-    freq := make(map[rune]int)
+    hashmap := make(map[rune]int)
 
-
-    for _, chx := range s {
-        freq[chx]++
+    for _, ch := range s {
+        hashmap[ch]++
     }
 
     for _, ch := range t {
-        freq[ch]--
+        hashmap[ch]--
 
-        if freq[ch] < 0 {
+        if hashmap[ch] < 0 {
             return false
-        }   
+        }
     }
 
-
     return true
+
 }
